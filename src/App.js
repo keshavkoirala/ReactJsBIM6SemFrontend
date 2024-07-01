216cd4f21d01fbc7db2000aa3ce771e51f9148f3
keshav.koirala <keshavkoiralait@gmail.com>
2024-07-01 11:00:50 +0545

    changes made to project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from './Button';
-import DemoComponent from './DemoComponent';
 import UserRegistrationForm from './User/UserRegistrationForm';
 import Login from './User/Login';
 import UserList from './User/UserList';
-import Navigation from './User/Navigation';
+// import Navigation from './User/Navigation';
 import { useState } from 'react';
 import HomePage from './User/HomePage';
-import ParentComponent from './PassingProps/ParentComponent';
-import StateManupulation from './PassingProps/StateManupulation';
-import UseEffectExample from './UseEffectExample';
-import MainComponent from './ReactContextExample/MainComponent';
 
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Category from './User/Category/CategoryForm';
+
 import CategoryList from './User/Category/CategoryList';
-import Parent from './ToDO/Parent';
-import UseNavigateHooksExample from './ReactRouterHooks/UseNavigateHooksExample';
+
 
 function App() {
 
@@ -31,9 +24,6 @@ function App() {
           <Route path='/userlist' element={<UserList />} />
           <Route path='/login' element={<Login />} />
           <Route path='/category' element={<CategoryList />} />
-          
-          <Route path='/navigateExample' element={<UseNavigateHooksExample />} />
-
           <Route path='*' element={<>URL Not Found</>} />
           
         </Routes>
